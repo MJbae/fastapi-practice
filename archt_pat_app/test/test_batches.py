d0c9f30959a80a36bfefa837fdeb637c36715503
maxwell <maxwell@aidkr.com>
2022-03-03 10:37:08 +0900

    test: check if custom exception raises

--- archt_pat_app/test/test_batches.py
+++ archt_pat_app/test/test_batches.py
@@ -2,7 +2,7 @@ from datetime import date, timedelta
 
 import pytest
 
-from archt_pat_app.domain.model import Batch, OrderLine, allocate
+from archt_pat_app.domain.model import Batch, OrderLine, allocate, OutOfStock
 
 today = date.today()
 tomorrow = today + timedelta(days=1)
@@ -81,3 +81,11 @@ def test_returns_allocated_batch_ref():
     line = OrderLine("oref", "HIGHBROW-POSTER", 10)
     allocation = allocate(line, [in_stock_batch, shipment_batch])
     assert allocation == in_stock_batch.reference
+
+
+def test_raises_out_of_stock_exception_if_cannot_allocate():
+    batch = Batch("batch1", "SMALL-FORK", 10, eta=today)
+    allocate(OrderLine("order1", "SMALL-FORK", 10), [batch])
+
+    with pytest.raises(OutOfStock, match="SMALL-FORK"):
+        allocate(OrderLine("order2", "SMALL-FORK", 1), [batch])
