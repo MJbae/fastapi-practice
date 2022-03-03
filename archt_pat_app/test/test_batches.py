e1bb8ac5057e16230354cdd3df19277bf50273a5
maxwell <maxwell@aidkr.com>
2022-03-03 10:31:55 +0900

    test: check if orderline can be allocated in multi batches

--- archt_pat_app/test/test_batches.py
+++ archt_pat_app/test/test_batches.py
@@ -2,7 +2,7 @@ from datetime import date, timedelta
 
 import pytest
 
-from archt_pat_app.domain.model import Batch, OrderLine
+from archt_pat_app.domain.model import Batch, OrderLine, allocate
 
 today = date.today()
 tomorrow = today + timedelta(days=1)
@@ -51,9 +51,33 @@ def test_can_only_deallocate_allocated_lines():
     assert batch.available_quantity == 20
 
 
-def test_prefers_warehouse_batches_to_shipments():
-    pytest.fail("todo")
+def test_prefers_current_stock_batches_to_shipments():
+    in_stock_batch = Batch("in-stock-batch", "RETRO-CLOCK", 100, eta=None)
+    shipment_batch = Batch("shipment-batch", "RETRO-CLOCK", 100, eta=tomorrow)
+    line = OrderLine("oref", "RETRO-CLOCK", 10)
+
+    allocate(line, [in_stock_batch, shipment_batch])
+
+    assert in_stock_batch.available_quantity == 90
+    assert shipment_batch.available_quantity == 100
 
 
 def test_prefers_earlier_batches():
-    pytest.fail("todo")
+    earliest = Batch("speedy-batch", "MINIMALIST-SPOON", 100, eta=today)
+    medium = Batch("normal-batch", "MINIMALIST-SPOON", 100, eta=tomorrow)
+    latest = Batch("slow-batch", "MINIMALIST-SPOON", 100, eta=later)
+    line = OrderLine("order1", "MINIMALIST-SPOON", 10)
+
+    allocate(line, [medium, earliest, latest])
+
+    assert earliest.available_quantity == 90
+    assert medium.available_quantity == 100
+    assert latest.available_quantity == 100
+
+
+def test_returns_allocated_batch_ref():
+    in_stock_batch = Batch("in-stock-batch-ref", "HIGHBROW-POSTER", 100, eta=None)
+    shipment_batch = Batch("shipment-batch-ref", "HIGHBROW-POSTER", 100, eta=tomorrow)
+    line = OrderLine("oref", "HIGHBROW-POSTER", 10)
+    allocation = allocate(line, [in_stock_batch, shipment_batch])
+    assert allocation == in_stock_batch.reference
