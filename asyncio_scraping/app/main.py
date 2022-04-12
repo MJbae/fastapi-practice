6c8f305a590172fad7119e3c04b101f641b94bbf
maxwell <maxwell@aidkr.com>
2022-04-12 16:34:30 +0900

    feat: add db connecting disconnecting methods

--- asyncio_scraping/app/main.py
+++ asyncio_scraping/app/main.py
@@ -1,8 +1,9 @@
-import re
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from pathlib import Path
+
+from app.models import mongodb
 
 BASE_DIR = Path(__file__).resolve().parent
 
@@ -25,3 +26,15 @@ async def search(request: Request, s: str):
         "./index.html",
         {"request": request, "title": "practice fastapi"}
     )
+
+
+@app.on_event("startup")
+def on_app_start():
+    """before app starts"""
+    mongodb.connect()
+
+
+@app.on_event("shutdown")
+def on_app_shutdown():
+    """after app shutdown"""
+    mongodb.close()
