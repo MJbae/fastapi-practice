e045d8049a11565d667001bd5bf4f82558f5ff28
maxwell <maxwell@aidkr.com>
2022-04-13 09:50:30 +0900

    chore: check if book model can insert in mongo db

--- asyncio_scraping/app/main.py
+++ asyncio_scraping/app/main.py
@@ -4,6 +4,7 @@ from fastapi.templating import Jinja2Templates
 from pathlib import Path
 
 from app.models import mongodb
+from app.models.book import Book
 
 BASE_DIR = Path(__file__).resolve().parent
 
@@ -13,6 +14,9 @@ templates = Jinja2Templates(directory=BASE_DIR / "templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def root(request: Request):
+    book = Book(keyword="파이썬", publisher="BJPublic",
+                price=1200, image="me.png", hhh="askljdlkas")
+    print(await mongodb.engine.save(book))  # DB에 저장
     return templates.TemplateResponse(
         "./index.html",
         {"request": request, "title": "콜렉터 북북이"},
