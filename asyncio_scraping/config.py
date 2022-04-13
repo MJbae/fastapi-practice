8d532e931b0119eace3332b52ea09808edb2354f
maxwell <maxwell@aidkr.com>
2022-04-13 09:00:51 +0900

    feat: define mongodb constants

--- asyncio_scraping/config.py
+++ asyncio_scraping/config.py
@@ -1,4 +1,5 @@
 import json
+from urllib.parse import quote_plus
 from pathlib import Path
 from typing import Optional
 
@@ -22,7 +23,13 @@ def get_secret(
         raise EnvironmentError(f"Set the {key} environment variable.")
 
 
-MONGO_URL = get_secret("MONGO_URL")
+MONGO_BASE_URL = get_secret("MONGO_BASE_URL")
+MONGO_USER = get_secret("MONGO_USER")
+MONGO_PASSWORD = get_secret("MONGO_PASSWORD")
+MONGO_URL_DETAIL = get_secret("MONGO_URL_DETAIL")
 MONGO_DB_NAME = get_secret("MONGO_DB_NAME")
 NAVER_API_ID = get_secret("NAVER_API_ID")
 NAVER_API_SECRET = get_secret("NAVER_API_SECRET")
+MONGO_URI = MONGO_BASE_URL + \
+    quote_plus(MONGO_USER) + ":" + \
+    quote_plus(MONGO_PASSWORD) + MONGO_URL_DETAIL
